e666d372725b8ba41b56de807afd335340a6afae
Tony Collett <70442911+TonyCollett@users.noreply.github.com>
2023-11-23 15:59:20 +1030

    image uploading!

--- DexMasterLibrary/DataAccess/Interfaces/IImageAssetData.cs
+++ DexMasterLibrary/DataAccess/Interfaces/IImageAssetData.cs
@@ -4,5 +4,6 @@ public interface IImageAssetData
 {
     Task<ImageAsset> GetImageAssetByIdAsync(string id);
     Task<ImageAsset> GetImageAssetByNameAsync(string name);
-    Task SaveImageAssetAsync(ImageAsset imageAsset);
+    Task UpsertImageAssetAsync(ImageAsset imageAsset);
+    Task SaveMultipleImageAssetAsync(IEnumerable<ImageAsset> imageAssets);
 }
--- DexMasterLibrary/DataAccess/MongoImageAssetData.cs
+++ DexMasterLibrary/DataAccess/MongoImageAssetData.cs
@@ -6,19 +6,20 @@ public class MongoImageAssetData : IImageAssetData
 
     public MongoImageAssetData(IDbConnection db)
     {
-        _imageAssets = db.ImageAssetCollection; // Assuming ImageAssetCollection is defined in IDbConnection
+        _imageAssets = db.ImageAssetCollection;
     }
 
-    public async Task<ImageAsset> GetImageAssetByIdAsync(string id) => await _imageAssets.Find(img => img.Id == id).FirstOrDefaultAsync();
+    public async Task<ImageAsset> GetImageAssetByIdAsync(string id) 
+        => await _imageAssets.Find(img => img.Id == id).FirstOrDefaultAsync();
 
-    public async Task<ImageAsset> GetImageAssetByNameAsync(string name) => await _imageAssets.Find(img => img.Name == name).FirstOrDefaultAsync();
+    public async Task<ImageAsset> GetImageAssetByNameAsync(string name) 
+        => await _imageAssets.Find(img => img.Name == name).FirstOrDefaultAsync();
 
-    public async Task SaveImageAssetAsync(ImageAsset imageAsset)
-    {
-        await _imageAssets.ReplaceOneAsync(
-            img => img.Id == imageAsset.Id,
-            imageAsset,
-            new ReplaceOptions { IsUpsert = true }
-        );
-    }
+    public async Task UpsertImageAssetAsync(ImageAsset imageAsset)
+        => await _imageAssets.ReplaceOneAsync(img => img.Id == imageAsset.Id, imageAsset, new ReplaceOptions { IsUpsert = true });
+    
+    
+    public async Task SaveMultipleImageAssetAsync(IEnumerable<ImageAsset> imageAssets)
+        => await _imageAssets.InsertManyAsync(imageAssets);
+
 }
