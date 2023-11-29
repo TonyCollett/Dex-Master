d39b5f5e84e885ef7759f64a975eeb8c0ea4cb1a
Tony Collett <70442911+TonyCollett@users.noreply.github.com>
2023-11-29 13:19:43 +1030

    image upload working

--- DexMasterLibrary/Models/ImageAsset.cs
+++ DexMasterLibrary/Models/ImageAsset.cs
@@ -6,5 +6,6 @@ public class ImageAsset
     [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; }
     public string Name { get; set; }
+    public string ContentType { get; set; }
     public byte[] Data { get; set; }
 }
